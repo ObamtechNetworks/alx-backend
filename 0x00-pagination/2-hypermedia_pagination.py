f1c39ec6396dab44928ff36f70a8d77a43011931
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-08-19 03:13:29 +0100

    refactor code

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -22,7 +22,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
         to return in a list for the pagination parameters
     """
     # configure the page indexes (start and end)
-    start_index = (page * page_size) - page_size
+    start_index = (page - 1) * page_size
     end_index = page * page_size
 
     return (start_index, end_index)
