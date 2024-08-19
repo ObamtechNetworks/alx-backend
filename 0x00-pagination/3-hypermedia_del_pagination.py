0ff70db0026aecc057e3ff86fd11018892afb3ce
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-08-19 23:40:15 +0100

    hypermedia del pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -4,13 +4,11 @@ Deletion-resilient hypermedia pagination
 """
 
 import csv
-import math
-from typing import List
+from typing import List, Dict
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -18,46 +16,24 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset."""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
-
+            self.__dataset = dataset[1:]  # Skip header
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0
-        """
+        """Dataset indexed by sorting position, starting at 0."""
         if self.__indexed_dataset is None:
             dataset = self.dataset()
-            truncated_dataset = dataset[:1000]
-            self.__indexed_dataset = {
-                i: dataset[i] for i in range(len(dataset))
-            }
+            self.__indexed_dataset = {i: dataset[i]
+                                      for i in range(len(dataset))}
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-        """Returns a dictionary with the following:
-        index: the current start index of the return page ->
-        index of first item in the current page.
-        for example if the requesting page is 3 with page_size 20, and no data
-        was removed from the dataset, the current index should be 60.
-        next_index: the next index to query with. That should be the index of
-        the first item after the last item on the current page.
-        page: size the current page size
-        data: the actual page of the data set
-
-        Args:
-            index (int, optional): current start index of the dafault page.
-            Defaults to None.
-            page_size (int, optional): the current page size. Defaults to 10.
-
-        Returns:
-            Dict: A dict
-        """
+        """Deletion-resilient hypermedia pagination."""
         # Step 1: Ensure the index is within valid range
         assert index is not None and 0 <= index < len(self.indexed_dataset())
 
