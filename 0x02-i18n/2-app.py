3711b662a15292c279bffb0b91d8f06127503372
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-08-28 03:54:23 +0100

    didn't add html

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""A simple flask app
+"""
+
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+# Create a Config class to configure app languages and default settings
+class Config:
+    """Configures available languages, locale, and timezone"""
+    LANGUAGES = ["en", "fr"]  # Supported languages
+    BABEL_DEFAULT_LOCALE = "en"  # Default language
+    BABEL_DEFAULT_TIMEZONE = "UTC"  # Default timezone
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Gets default locale"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+# Define routes
+@app.route('/')
+def home():
+    """A default home route"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
