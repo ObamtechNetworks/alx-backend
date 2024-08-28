476d83e362235af705e58e2cb421216fe012bbf4
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-08-28 03:11:27 +0100

    simple flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""A simple flask app
+"""
+
+
+from flask import Flask
+from flask_babel import Babel
+
+app = Flask(__name__)
+# app.config.from_pyfile('mysettings.cfg')
+# babel = Babel(app)
+
+
+# define routes
+@app.route('/')
+def home():
+    """"A default home route"""
+    return render_template('0-index.html')
