f0a4c1b504a185aa4fbdc3c9d5f0aef84f4d1f4e
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-08-28 03:52:04 +0100

    reset code

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,11 +6,6 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-app = Flask(__name__)
-
-# Instantiate Babel with the Flask app
-babel = Babel(app)
-
 
 # Create a Config class to configure app languages and default settings
 class Config:
@@ -20,8 +15,16 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = "UTC"  # Default timezone
 
 
-# Apply the Config class to the app's configuration
+app = Flask(__name__)
 app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Gets default locale"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 # Define routes
