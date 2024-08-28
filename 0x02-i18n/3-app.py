47566a07d0c7843f421d2b18c555a302ef8f839b
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-08-28 04:13:41 +0100

    internalization

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,37 +1,38 @@
 #!/usr/bin/env python3
-"""A simple flask app
+"""Flask app with Babel translations setup
 """
 
-
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, _
 
 
 # Create a Config class to configure app languages and default settings
 class Config:
     """Configures available languages, locale, and timezone"""
-    LANGUAGES = ["en", "fr"]  # Supported languages
-    BABEL_DEFAULT_LOCALE = "en"  # Default language
-    BABEL_DEFAULT_TIMEZONE = "UTC"  # Default timezone
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+# Create Flask app instance
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# Initialize Babel
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
-    """Gets default locale"""
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    """Select a language translation"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 # Define routes
 @app.route('/')
 def home():
     """A default home route"""
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
